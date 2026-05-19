1ea0f4f060cacd60e3774795bb642c7a5ea5034e
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-05-19 12:22:14 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" <<'\n';  //std= standdard , cout = character out <<= output operator , '\n' = new line character
+    std::cout << "Welcome to c++ programming." <<std::endl; //std::endl = end line it flushes the output buffer and moves to the next line
+    return 0;
+}       
